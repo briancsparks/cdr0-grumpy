31256ab2b70b255df1ea5bba52fe4e64085caa6a
Brian C Sparks <briancsparks@gmail.com>
2020-12-31 01:06:01 -0800

    independence from local-polyrepo

--- polyrepo/cdr0-sg.js
+++ polyrepo/cdr0-sg.js
@@ -1,4 +1,5 @@
 
-const polyPkgs = ['@cdr0/sg'];
+// const polyPkgs = ['@cdr0/sg'];
+const polyPkgs = [];
 
-module.exports = require('./get-local-polyrepo')(__dirname, polyPkgs)('@cdr0/sg');
+module.exports = require('./get-local-polyrepo')(__dirname, {polyPkgs})('@cdr0/sg') || require('@cdr0/sg');
